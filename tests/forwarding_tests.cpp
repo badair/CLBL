b4e72bddc9efac59b16e4dee7264ad774dc5e036
badair <badair@harding.edu>
2015-12-06 06:38:27 -0600

    more forwarding tests

--- tests/forwarding_tests.cpp
+++ tests/forwarding_tests.cpp
@@ -12,26 +12,33 @@ namespace fwd_tests {
 
     struct copy_counter {
 
+        //increment value on every copy
         static int value;
+        static void reset() { value = 0; }
 
         copy_counter() = default;
-
-        copy_counter(const copy_counter&) {
-            ++value;
-        }
-
-        copy_counter(copy_counter&) {
-            ++value;
-        }
-
+        copy_counter(const copy_counter&) { ++value; }
+        copy_counter(copy_counter&) { ++value; }
         copy_counter(copy_counter&&) = default;
     };
 
-    void take_copy(copy_counter) {
+    /*
+    calling the take_copy function will increment
+    the copy_counter::value. This will allow us to
+    confirm that arguments passing through CLBL are
+    not copied superfluously
+    */
+    void take_copy(copy_counter) {}
+    
+    /*
+    we will use this to test something that forwards what 
+    we're forwarding
+    */
+    auto forwarder = [](auto&& t) {return take_copy(t);};
 
-    }
-
-    auto t_forwarder = [](auto&& t) {return take_copy(t);};
+    struct copy_taker{
+        void take_copy(copy_counter) {}
+    };
 }
 
 int fwd_tests::copy_counter::value = 0;
@@ -44,7 +51,7 @@ void forwarding_tests() {
     using copy_counter = fwd_tests::copy_counter;
 
     {
-        copy_counter::value = 0;
+        copy_counter::reset();
 
         auto f = func(&fwd_tests::take_copy);
         copy_counter obj{};
@@ -55,31 +62,85 @@ void forwarding_tests() {
     }
 
     {
-        copy_counter::value = 0;
+        copy_counter::reset();
 
-        auto f = func(&fwd_tests::t_forwarder);
+        auto f = func(&fwd_tests::forwarder);
         copy_counter obj{};
         f(obj);
 
         TEST(copy_counter::value == 1);
     }
     {
-        copy_counter::value = 0;
+        copy_counter::reset();
 
-        auto f = func(fwd_tests::t_forwarder);
+        auto f = func(fwd_tests::forwarder);
         copy_counter obj{};
         f(obj);
 
         TEST(copy_counter::value == 1);
     }
     {
-        copy_counter::value = 0;
+        copy_counter::reset();
 
-        auto f = func(std::ref(fwd_tests::t_forwarder));
+        auto f = func(std::ref(fwd_tests::forwarder));
         copy_counter obj{};
         f(obj);
 
         TEST(copy_counter::value == 1);
+    }
+    {
+        copy_counter::reset();
+        copy_counter obj{};
+
+        auto f = func(&fwd_tests::take_copy);
+        auto hardened = harden<void(copy_counter)>(f);
+
+        hardened(obj);
+        TEST(copy_counter::value == 1);
+
+        auto std_func = convert_to<std::function>(hardened);
+
+        std_func(obj);
+        TEST(copy_counter::value == 2);
+    }
+    {
+        copy_counter::reset();
+        copy_counter obj{};
+
+        auto f = func(fwd_tests::forwarder);
+        auto hardened = harden<void(copy_counter)>(f);
+
+        hardened(obj);
+        TEST(copy_counter::value == 1);
+
+        auto std_func = convert_to<std::function>(hardened);
+
+        std_func(obj);
+        TEST(copy_counter::value == 2);
+    }
+    {
+        copy_counter::reset();
+        copy_counter obj{};
+
+        auto std_func = convert_to<std::function>(harden<void(copy_counter)>(func(fwd_tests::forwarder)));
+
+        std_func(obj);
+        TEST(copy_counter::value == 1);
+    }
+    {
+        copy_counter::reset();
+        copy_counter obj{};
+
+        auto f = func(fwd_tests::copy_taker{}, &fwd_tests::copy_taker::take_copy);
+        auto hardened = harden<void(copy_counter)>(f);
+
+        hardened(obj);
+        TEST(copy_counter::value == 1);
+
+        auto std_func = convert_to<std::function>(hardened);
+
+        std_func(obj);
+        TEST(copy_counter::value == 2);
     }
 
 #endif
